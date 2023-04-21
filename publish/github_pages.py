44591171501b9bb243b70fdddadf23e5b3fed6ed
Daniel St <daniel.steinmaurer@htl-braunau.at>
2023-04-21 19:16:33 +0200

    finish up autocompile

--- publish/github_pages.py
+++ publish/github_pages.py
@@ -2,7 +2,7 @@ from publish.paths import export_path
 from ghp_import import ghp_import
 
 def main(only_local=False)->int:
-    ghp_import(str(export_path.absolute()), nojekyll=True, force=True, only_local=True)
+    ghp_import(str(export_path.absolute()), nojekyll=True, force=True, push=not only_local)
     return 0
 
 if __name__ == '__main__':
